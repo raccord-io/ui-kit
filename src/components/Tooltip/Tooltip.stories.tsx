a25f88192c8a0b00b7f150c093217d4c3e751be6
Clément FERNANDES <clement.fernandes@epitech.eu>
2024-05-27 15:31:41 +0900

    feat(tooltip): created a tooltip that can display in every placement existing

--- src/components/Tooltip/Tooltip.stories.tsx
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -4,8 +4,53 @@ export default { title: 'Components/Tooltip', component: Tooltip };
 
 export const _Tooltip = {
   render: () => (
-    <>
-      <Tooltip content={'Hello I am tooltip content'} />
-    </>
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex items-center justify-center mb-4">
+        <Tooltip content="This is a tooltip" placement="top-start">
+          TOP-START
+        </Tooltip>
+        <Tooltip content="This is a tooltip" placement="top">
+          TOP
+        </Tooltip>
+        <Tooltip content="This is a tooltip" placement="top-end">
+          TOP-END
+        </Tooltip>
+      </div>
+      <div className="flex justify-between w-full max-w-screen-md mb-4">
+        <div className="flex flex-col items-start">
+          <Tooltip content="This is a tooltip" placement="left-start">
+            LEFT-START
+          </Tooltip>
+          <Tooltip content="This is a tooltip" placement="left">
+            LEFT
+          </Tooltip>
+          <Tooltip content="This is a tooltip" placement="left-end">
+            LEFT-END
+          </Tooltip>
+        </div>
+        <div className="flex flex-col items-end">
+          <Tooltip content="This is a tooltip" placement="right-start">
+            RIGHT-START
+          </Tooltip>
+          <Tooltip content="This is a tooltip" placement="right">
+            RIGHT
+          </Tooltip>
+          <Tooltip content="This is a tooltip" placement="right-end">
+            RIGHT-END
+          </Tooltip>
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <Tooltip content="This is a tooltip" placement="bottom-start">
+          BOTTOM-START
+        </Tooltip>
+        <Tooltip content="This is a tooltip" placement="bottom">
+          BOTTOM
+        </Tooltip>
+        <Tooltip content="This is a tooltip" placement="bottom-end">
+          BOTTOM-END
+        </Tooltip>
+      </div>
+    </div>
   ),
 };
--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,34 +1,61 @@
 import { ComponentPropsWithoutRef, cloneElement, useState } from 'react';
 import { FiHelpCircle } from 'react-icons/fi';
 
+const placementClasses = {
+  'top-start': 'bottom-full left-0 mb-2',
+  top: 'bottom-full left-1/2 transform -translate-x-1/2 mb-2',
+  'top-end': 'bottom-full right-0 mb-2',
+  'right-start': 'left-full top-0 ml-2',
+  right: 'left-full top-1/2 transform -translate-y-1/2 ml-2',
+  'right-end': 'left-full bottom-0 ml-2',
+  'bottom-start': 'top-full left-0 mt-2',
+  bottom: 'top-full left-1/2 transform -translate-x-1/2 mt-2',
+  'bottom-end': 'top-full right-0 mt-2',
+  'left-start': 'right-full top-0 mr-2',
+  left: 'right-full top-1/2 transform -translate-y-1/2 mr-2',
+  'left-end': 'right-full bottom-0 mr-2',
+};
+
 export interface TooltipProps extends ComponentPropsWithoutRef<'div'> {
-  size: string;
+  content: string;
+  placement?: keyof typeof placementClasses;
+  customClass?: string;
+  children?: React.ReactNode;
 }
 
-export function Tooltip({ ...props }) {
-  const { content, customClass, ...rest } = props;
+export function Tooltip(props: TooltipProps) {
+  const {
+    content,
+    placement = 'bottom',
+    customClass,
+    children,
+    ...rest
+  } = props;
 
   const [showTooltip, setShowTooltip] = useState(false);
   const icon = <FiHelpCircle />;
   const clonedIcon = cloneElement(icon, {
     className: 'stroke-current text-neutral',
   });
+
   return (
     <div
       data-testid="Tooltip"
-      className="w-fit p-2 hover:cursor-pointer"
+      className="inline-flex items-center p-2 hover:cursor-pointer relative"
       {...rest}
       onMouseEnter={() => setShowTooltip(true)}
       onMouseLeave={() => setShowTooltip(false)}
     >
       {clonedIcon}
-      {showTooltip && (
-        <div
-          className={`w-fit z-10 absolute bg-tertiary p-3 rounded-lg text-neutral ml-2 ${customClass} `}
-        >
-          {content}
-        </div>
-      )}
+      <span className="ml-2 text-neutral">{children}</span>
+      <div
+        className={`w-fit z-10 absolute bg-tertiary p-3 rounded-lg text-neutral opacity-0transition-opacity
+        duration-300 ${customClass} ${placementClasses[placement]} ${
+          showTooltip ? 'opacity-90' : 'opacity-0'
+    }`}
+      >
+        {content}
+      </div>
     </div>
   );
 }
