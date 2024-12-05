b22cbdaf3675df7019cb6269c042477e28d62c6d
Maxime CARABINA <maxime.carabina@epitech.eu>
2024-12-05 15:06:52 +0100

    feat(components): added some new components

---
+++ src/components/Avatar/Avatar.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Avatar, AvatarImage, AvatarFallback } from './Avatar';
+
+const meta = {
+  title: 'Components/Avatar',
+  component: Avatar,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Avatar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+      </>
+    ),
+  },
+};
