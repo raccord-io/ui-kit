2d61c54e3e38ce04cb0373f308e73867a5a008e4
Maxime Carabina <72019005+maxime-carabina@users.noreply.github.com>
2025-01-14 15:42:38 +0100

    feat(path): remaned path symbol (#86)

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -6,9 +6,6 @@ import { Slot } from '@radix-ui/react-slot';
 import { VariantProps, cva } from 'class-variance-authority';
 import { PanelLeft } from 'lucide-react';
 
-import { useIsMobile } from '@/hooks';
-import { cn } from '@/lib/utils';
-
 import {
   Button,
   Input,
@@ -21,6 +18,8 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '../';
+import { useIsMobile } from '../../hooks';
+import { cn } from '../../lib/utils';
 
 const SIDEBAR_COOKIE_NAME = 'sidebar:state';
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
